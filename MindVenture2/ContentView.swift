6c9dec911815cfc6ce9ee44479346699fe7ff2ca
alcaraz728 <alcaraz4@illinois.edu>
2023-06-13 14:37:54 -0500

    hi1

--- MindVenture2/ContentView.swift
+++ MindVenture2/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
+    //hiiiii
 
     var body: some View {
         NavigationView {
