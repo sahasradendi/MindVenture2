c193b583fba5dc0000cf8fd42cd363ab15e52675
alcaraz728 <alcaraz4@illinois.edu>
2023-06-14 21:09:19 -0500

    created the link page

---
+++ MindVenture2/LinksPage.swift
@@ -0,0 +1,20 @@
+//
+//  LinksPage.swift
+//  MindVenture2
+//
+//  Created by Veronica Alcaraz on 6/14/23.
+//
+
+import SwiftUI
+
+struct LinksPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LinksPage_Previews: PreviewProvider {
+    static var previews: some View {
+        LinksPage()
+    }
+}
